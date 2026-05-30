dba1d018afa29d8cd15bfad81567d049dc20f402
KaneVina <wtskane@gmail.com>
2026-05-30 21:30:04 +0700

    fix: restore DBContext to per-call connection creation after merge

--- src/main/java/utils/DBContext.java
+++ src/main/java/utils/DBContext.java
@@ -3,50 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package utils;
-
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author PHUC KHANG
  */
 public class DBContext {
-    protected Connection conn = null;
-
-    public DBContext() {
+    public Connection getConnection() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String dbURL = "jdbc:sqlserver://localhost:1433;"
-                    + "databaseName=dbProductManagement;"
+                    + "databaseName=BookstoreDB;"
                     + "user=sa;"
                     + "password=123456;"
                     + "encrypt=true;trustServerCertificate=true;";
-            conn = DriverManager.getConnection(dbURL);
-            if (conn != null) {
-                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
-                System.out.println("Driver name: " + dm.getDriverName());
-                System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: "
-                        + dm.getDatabaseProductName());
-                System.out.println("Product version: "
-                        + dm.getDatabaseProductVersion());
-            }else{
-                System.out.println("NULL");
-            }
-        } catch (SQLException ex) {
-            System.out.println("Khong ket noi duoc");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            return DriverManager.getConnection(dbURL);
+        } catch (Exception e) {
+            System.out.println("Khong ket noi duoc: " + e.getMessage());
+            return null;
         }
     }
 
     public static void main(String[] args) {
         DBContext db = new DBContext();
+        Connection conn = db.getConnection();
+        System.out.println(conn != null ? "Ket noi thanh cong" : "That bai");
     }
-  
 }
